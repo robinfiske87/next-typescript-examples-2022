397101041015342a9f8a0d5e43e6d96265d71629
RobinRF <robinfiske87@gmail.com>
2022-12-27 12:02:10 +0100

    added state for storing data in index. Passed to form. If form is submitted, the states are stored in an array. Added some buttons for handling stored data

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,34 +1,52 @@
 import React, { useState } from 'react';
 import { Input, PrimaryButton, SecondaryButton } from './reusableComponents';
 import { z } from "zod";
+import type { StoredData } from '../pages/index';
 
-const Form: React.FC = () => {
+// Dispatch type for setting state with a new value or by a function that receives the previous state and returns a new state value.
+type Dispatch<A> = (value: A) => void;
+type SetStateAction<S> = S | ((prevState: S) => S);
+
+// Props for the form component.
+type FormProps = {
+  setStoredData: Dispatch<SetStateAction<StoredData>>; // Function to set the stored data state in the parent component.
+};
+
+// Form component.
+const Form: React.FC<FormProps> = (props) => {
+  // Destructure the setStoredData prop from the props object.
+  const { setStoredData } = props;
+  
+  // Use state for the form data.
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     message: ''
   });
+  // Use state for the stored form data.
   const [storedForm, setStoredForm] = useState({
     name: '',
     email: '',
     message: ''
   });
+  // Use state for form validation errors.
   const [errors, setErrors] = useState<string[]>([]);
 
-// Schema for form validaton. Name can be any string between 1 and 40 characters long, email must be a valid email address and message can be any string between 1 and 100 characters long.
+  // Schema for form data validation.
   const formDataSchema = z.object({
-    name: z.string().min(1).max(40).regex(/^[a-zA-Z]+(\s[a-zA-Z]+)?$/),
-    email: z.string().email(),
-    message: z.string().min(1).max(100)
+    name: z.string().min(1).max(40).regex(/^[a-zA-Z]+(\s[a-zA-Z]+)?$/), // Name can be any string between 1 and 40 characters long.
+    email: z.string().email(), // Email must be a valid email address.
+    message: z.string().min(1).max(100) // Message can be any string between 1 and 100 characters long.
   });
-  
 
+    // Event handler for form input changes.
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    };
+  
+    // Event handler for form submission.
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // Check if all fields are empty and return if true
     if (Object.values(formData).every(val => val === '')) return setErrors([]);
@@ -36,7 +54,9 @@ const Form: React.FC = () => {
       const validatedFormData = formDataSchema.parse(formData);
       setStoredForm(validatedFormData);
       setErrors([]);
-      // submit validatedFormData to server or do something else with it here
+      // submit validatedFormData to server or do something else with it here. I am just storing it in a state array in the index.tsx file for now. It can be displayed in the console or removed with the presented buttons.
+      setStoredData((prevStoredData) => ({ ...prevStoredData, data: [...prevStoredData.data, validatedFormData] }));
+
     } catch (error ) {
       if(error instanceof z.ZodError)
       setErrors(error.issues.map(issue => issue.message));
@@ -62,8 +82,13 @@ const Form: React.FC = () => {
 
   return (
     <>
+      {/* Form header */}
       <h2 className="text-2xl font-bold mb-5 ml-1">Form</h2>
+
+      {/* Form element */}
       <form onSubmit={handleSubmit} >
+
+        {/* Input for name */}
         <Input
           label='Name'
           name="name"
@@ -71,6 +96,8 @@ const Form: React.FC = () => {
           onChange={handleChange}
           placeholder="Name"
         />
+
+        {/* Input for email */}
         <Input
           label='Email'
           name="email"
@@ -78,6 +105,8 @@ const Form: React.FC = () => {
           onChange={handleChange}
           placeholder="Email"
         />
+
+        {/* Input for message */}
         <Input
           label='Message'
           name="message"
@@ -85,14 +114,19 @@ const Form: React.FC = () => {
           onChange={handleChange}
           placeholder="Message"
         />
+
+        {/* Button to submit data to state, or use something else if you want it to persist */}
         <PrimaryButton>
           Submit
         </PrimaryButton>
+
+        {/* Button to clear the form. Only available if something is written in the form*/}
+        {Object.values(formData).some(val => val !== '') && 
         <SecondaryButton
           onClick={handleAbort}
         >
-          Abort
-        </SecondaryButton>
+          Clear Form
+        </SecondaryButton>}
       </form>
 
       {// Display errors if there are any
@@ -104,12 +138,14 @@ const Form: React.FC = () => {
         })}
       </ul>
       )}
+
+      {/* This is the currently stored form. Previous forms are stored in another array if not cleared.  */}
       {!Object.values(storedForm).every(val => val === '') && (
-          <div>
+          <div className='m-1 overflow-auto max-w-sm'>
             <h2 className="text-2xl font-bold">Stored Form Data</h2>
-            <p>Name: {storedForm.name}</p>
-            <p>Email: {storedForm.email}</p>
-            <p>Message: {storedForm.message}</p>
+            <p className="whitespace-normal text-wrap">Name: {storedForm.name}</p>
+            <p className="whitespace-normal text-wrap">Email: {storedForm.email}</p>
+            <p className="whitespace-normal text-wrap">Message: {storedForm.message}</p>
           </div>
         )}
     
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { NavigationButton } from '../components/reusableComponents';
+import { SecondaryButton, NavigationButton } from '../components/reusableComponents';
 import { Form } from '../components/form';
 
+export type StoredData = {
+  data: (string | { name: string; email: string; message: string; })[];
+};
 
 const Home: React.FC = () => {
+  // Declare a state variable called storedData with an initial value of an empty data array
+  const [storedData, setStoredData] = useState<StoredData>({ data: [] });
+
+  // Declare a function to reset the storedData state variable to an empty data array
+  function resetDataStore() {
+    setStoredData({ data: [] });
+  }
 
   return (
     <main className="mx-20 my-20 bg-indigo-50">
 
-      { // Form data is stored in the formData state variable in the form component. The form component is imported from the form.tsx file. I handle states in the form component.
-      
-      //I have also added a link to the examples page.
+      { // Form data is stored in the formData state variable in the form component. The data from the form is stored in a state. This is to exemplify state handling. The forms created are pushed to an array. I have added a button to log the data to the console. I have also added a button to reset the data store to an empty array.
       }
-      
-      <Form />
-    
+
+      <Form setStoredData={setStoredData} />
+
+      {/* Button to log the storedData state variable to the console */}
+      <SecondaryButton 
+        onClick={() => console.log(storedData)}>Log data
+      </SecondaryButton>
+
+      {/* Button to reset the storedData state variable to an empty data array */}
+      <SecondaryButton 
+        onClick={resetDataStore}>Remove Stored Data
+      </SecondaryButton>
+
+      {/* Button to navigate to the examples page */}
+      <br/>
       <NavigationButton >
         <Link href="/examples">To examples and API state handling</Link>
       </NavigationButton>
